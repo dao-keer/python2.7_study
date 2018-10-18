d344ab154bdcfd0b32ef072123dd6cb04548bf6c
dao-keer <yazhoucai@163.com>
2018-10-18 22:29:39 +0800

    feat:add new code

--- 8-5.py
+++ 8-5.py
@@ -6,3 +6,23 @@ def makeCake(name, *fruit):
 makeCake('xiaoming', 'apple', 'orange', 'banana')
 makeCake('dao-keer', 'pear', 'apple')
 makeCake('zz', 'banana')
+
+def userInfo(name, age, **info):
+	_info = {}
+	_info['name'] = name
+	_info['age'] = age
+	for key, value in info.items():
+		_info[key] = value
+	return _info
+
+print userInfo('dao-keer', 20, sex ='male')
+
+def makeCar(name, color, **info):
+	car = {}
+	car['name'] = name
+	car['color'] = color
+	for key, value in info.items():
+		car[key] = value
+	return car
+
+print makeCar('bmw', 'red', size=4, isOne=True)
